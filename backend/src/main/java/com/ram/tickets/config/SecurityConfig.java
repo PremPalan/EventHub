d18e68363b38047b9614f9151333e5a20220cf33
Ram Palan <ramrampalan@gmail.com>
2026-06-30 01:14:49 +0530

    Health status -2

--- backend/src/main/java/com/ram/tickets/config/SecurityConfig.java
+++ backend/src/main/java/com/ram/tickets/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
                                 .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
                                 .requestMatchers("/api/v1/ticket-validations").hasRole("STAFF")
+                                .requestMatchers("/health").permitAll()
                                 .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
